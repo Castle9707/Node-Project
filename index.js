f27a79f3d4b57850f34a990173dc65bc7bb04b19
Castle9707 <paomincc25@gmail.com>
2024-05-24 16:32:50 +0800

    0524青鳥行動

--- index.js
+++ index.js
@@ -2,12 +2,83 @@
 // console.log(process.env.DB_USER);
 
 import express from "express";
+import multer from "multer";
+
+const upload = multer({ dest: "tmp_uploads/" });
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+// 只會解析 application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// 只會解析 application/json
+app.use(express.json());
+
+// routes
+// 設定路由, 只允許用 GET 拜訪
 app.get('/', (req, res) => {
-  res.send(`<h2>哈囉哈</h2>`); //send預設輸出是HTML
+  //res.send(`<h2>哈囉哈</h2>`); //send預設輸出是HTML
+  res.render("home", { name: "Bob" });
 });
+
+app.get("/json-sales", (req, res) => {
+  const sales = [
+    {
+      name: "Bill",
+      age: 28,
+      id: "A001",
+    },
+    {
+      name: "Peter",
+      age: 32,
+      id: "A002",
+    },
+    {
+      name: "Carl",
+      age: 29,
+      id: "A003",
+    },
+  ];
+  res.render("json-sales", { sales });
+});
+
+app.get("/try-qs", (req, res) => {
+  res.json(req.query);// 查看 query string
+});
+
+app.get("/try-post-form", (req, res) => {
+  res.render("try-post-form");
+});
+
+//const urlencodedParser = express.urlencoded({ extended: true });
+app.post("/try-post-form", (req, res) => {
+  //res.json(req.body);
+  res.render("try-post-form", req.body);
+});
+
+app.post("/try-post", (req, res) => {
+  res.json(req.body);
+});
+
+app.post("/try-upload", upload.single('avatar'), (req, res) => {
+  res.json({
+    body: req.body,
+    file: req.file,
+  });
+});
+
+// 設定靜態內容資料夾
+app.use(express.static("public"));
+app.use("/bootstrap", express.static("node_modules/bootstrap/dist"));
+
+// ********* 要放在所有的路由設定之後
+// use 接受所有 HTTP 方法
+app.use((req, res) => {
+  res.type("text/plain").status(404).send("走錯路啦")
+});
+
 
 const port = process.env.WEB_PORT || 3002; // 若沒有設定，預設為3002
 app.listen(port, () => {
